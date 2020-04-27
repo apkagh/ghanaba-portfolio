004d3033d681663975905dfdf4a49d91e05104bd
Rails Test <apkagh@gmail.com>
2020-04-27 10:45:52 +0200

    added session tracker for dynamic source links

--- app/controllers/concerns/devise_whitelist.rb
+++ app/controllers/concerns/devise_whitelist.rb
@@ -4,10 +4,10 @@ module DeviseWhiteList
   included do
     before_filter :configure_permitted_parameters, if: :devise_controller
   end
-  
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     #devise_parameter_sanitizer.permit(:edit, keys: [:name])
-    #devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 end
