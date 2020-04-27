91a318361720900ee520e022b6ad4f5cd1391ab8
Rails Test <apkagh@gmail.com>
2020-04-27 11:25:25 +0200

    Refactored application controller to include concern for source feature

--- app/controllers/concerns/devise_whitelist.rb
+++ app/controllers/concerns/devise_whitelist.rb
@@ -7,7 +7,6 @@ module DeviseWhiteList
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    #devise_parameter_sanitizer.permit(:edit, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 end
